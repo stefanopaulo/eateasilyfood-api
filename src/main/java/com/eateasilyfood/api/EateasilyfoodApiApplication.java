e97e4ddb900482eaaefd5b6d4dcbcd6ccc0a5a97
spsouza <stefanopaulo47@gmail.com>
2023-09-04 18:43:48 -0300

    Start project

---
+++ src/main/java/com/eateasilyfood/api/EateasilyfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.eateasilyfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EateasilyfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EateasilyfoodApiApplication.class, args);
+	}
+
+}
